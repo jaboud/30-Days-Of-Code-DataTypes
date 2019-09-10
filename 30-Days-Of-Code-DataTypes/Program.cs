9b828c63f74929fcf9fe1bb7c4dcdcb7083f6a97
jaboud <jaboud94@gmail.com>
2019-09-10 14:43:41 +1000

    Declared varibles

--- 30-Days-Of-Code-DataTypes/Program.cs
+++ 30-Days-Of-Code-DataTypes/Program.cs
@@ -6,7 +6,10 @@ namespace _30_Days_Of_Code_DataTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variable declartion for an Integer, double and a string
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
         }
     }
 }
