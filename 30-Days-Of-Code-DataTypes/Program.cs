750e0938e966fbd0d68b2b3e5cef0e23640cf4aa
jaboud <jaboud94@gmail.com>
2019-09-10 14:35:13 +1000

    Created project solution

---
+++ 30-Days-Of-Code-DataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _30_Days_Of_Code_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
